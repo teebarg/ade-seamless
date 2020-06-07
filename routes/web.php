156d7ad38a5275820065f291273267b464bb9489
Beaft <teebarg01@gmail.com>
2020-06-07 07:53:16 +0100

    redirect to employee page

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return redirect('employee');
+});
 
 Route::resource('employee', 'EmployeeController');
-//Route::resource('/', 'EmployeeController');
